4f40ec5475c8af9d83ad84ed695340ace8b245df
akinalkan <103288322+akinalkan@users.noreply.github.com>
2022-06-21 20:32:20 +0300

    day08

---
+++ src/day08/S5_IfElseStatements.java
@@ -0,0 +1,24 @@
+package day08;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class S5_IfElseStatements {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Lütfen gün ismi yazınız");
+        String gun= scan.next().toLowerCase();
+        if(gun.equals("pazartesi")||gun.equals("salı")||gun.equals("çarşamba")||gun.equals("perşembe")
+                ||gun.equals("cuma")||gun.equals("cumartesi")||gun.equals("pazar")) {
+            System.out.println(""+gun.toUpperCase(Locale.ROOT).charAt(0)+gun.toLowerCase().charAt(1)+gun.toLowerCase().charAt(2));
+        }
+        else{
+            System.out.println("Lütfen geçerli bir gün yazınız");
+        }
+
+
+
+
+    }
+
+}
---
+++ src/day08/S6_IfElseStatements.java
@@ -0,0 +1,32 @@
+package day08;
+
+import java.util.Scanner;
+
+public class S6_IfElseStatements {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lütfen 1. sayıyı giriniz");
+        double sayi1=scan.nextDouble();
+
+        System.out.println("Lütfen 2. sayıyı giriniz");
+        double sayi2=scan.nextDouble();
+        if(sayi1>0 && sayi2>0){
+            System.out.println("Girilen sayılar toplamı: "+(sayi1+sayi2));
+        }
+        if(sayi1<0 && sayi2<0){
+            System.out.println("Girilen sayılar çarpımı :"+(sayi1*sayi2));
+        }
+        if((sayi1<0 &&sayi2>0) || (sayi1>0 && sayi2<0)){
+            System.out.println("Farklı işaretlerde sayılarla işlem yapamazsınız");
+        }
+        if(sayi1==0 ||sayi2==0){
+            System.out.println("sıfır çarpmaya göre yutan elemandır");
+        }
+
+
+
+
+
+
+    }
+}
