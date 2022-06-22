86f4c43ceeff5590b92098b035a9977b2558afd7
akinalkan <103288322+akinalkan@users.noreply.github.com>
2022-06-22 20:40:45 +0300

    day09

---
+++ src/day09/Practice6_Ascii.java
@@ -0,0 +1,16 @@
+package day09;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Practice6_Ascii {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        System.out.println("Lütfen bir karakter giriniz");
+        char harf = scan.next().charAt(0);
+        System.out.println(harf+0);
+
+
+
+    }
+}
