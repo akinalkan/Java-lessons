f0f54a1a3172faac5a6be82d2e450b5edbf8e673
akinalkan <103288322+akinalkan@users.noreply.github.com>
2022-06-24 23:22:45 +0300

    day11

---
+++ src/day11_stringManupulations/C04_contains.java
@@ -0,0 +1,34 @@
+package day11_stringManupulations;
+
+public class C04_contains {
+    public static void main(String[] args) {
+        /*
+         Kullanicidan bir cumle isteyin. Cumle “buyuk” kelimesi iceriyorsa
+          tum cumleyi buyuk harf olarak, “kucuk” kelimesi iceriyorsa tum cumleyi
+           kucuk harf olarak yazdirin, iki kelimeyi de icermiyorsa “Cumle kucuk
+           yada buyuk kelimesi icermiyor” yazdirin.
+         */
+        String cumle = "Java buyuk, dunya kucuk";
+        cumle = cumle.toLowerCase();
+
+        //requirements'de buyuk harf , kucuk harf hassasiyeti
+        // hakkında bir sey söylenmemiş
+        //ikinci olarak da her iki kelimeyi içerme durumu açıklanmamış
+        //bu durumda görevi bize kim verdiyse ona sormak lazım
+        //ek requirements :ikisi de içeriyorsa "karar ver buyuk mü yazdırayımi küçük mü?"
+        //case sensitive olmasın
+        if (cumle.contains("kucuk") && cumle.contains("buyuk")) {
+            System.out.println("Karar ver. Buyuk mu, kucuk mu yazdirayim?");
+
+        } else if (cumle.contains("kucuk")) {
+            System.out.println(cumle.toLowerCase());
+
+        } else if (cumle.contains("buyuk")) {
+            System.out.println(cumle.toUpperCase());
+
+        } else {
+            System.out.println("Cumle kucuk ya da buyuk kelimesi icermiyor.");
+        }
+
+    }
+}
