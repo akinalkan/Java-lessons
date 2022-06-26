8c23e2fd4ecf73e80aca06bfe6e35954e7388953
akinalkan <103288322+akinalkan@users.noreply.github.com>
2022-06-26 18:39:22 +0300

    work

---
+++ src/day12_stringManupulations/C06_subString.java
@@ -0,0 +1,23 @@
+package day12_stringManupulations;
+
+import java.util.Scanner;
+
+public class C06_subString {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Lütfen bir şifre giriniz");
+        String sifre=scan.nextLine();
+        String ilkHarf=sifre.substring(0,1);
+        String ilkHarfBuyuk=sifre.substring(0,1).toUpperCase();
+
+        String sonHarf=sifre.substring(sifre.length()-1);
+        String sonHarfKucuk=sifre.substring(sifre.length()-1).toLowerCase();
+        String boslukSifre=sifre.replaceAll("\\s","");
+        if(ilkHarf.equals(ilkHarfBuyuk) && sonHarf.equals(sonHarfKucuk) &&sifre.equals(boslukSifre)&&sifre.length()>=8){
+            System.out.println("Sifre basari ile tanimlandi");
+        }else{
+            System.out.println("Islem basarisiz,Lutfen yeni bir sifre girin");
+        }
+
+    }
+}
---
+++ src/day12_stringManupulations/C07_webSorusu.java
@@ -0,0 +1,27 @@
+package day12_stringManupulations;
+
+import java.util.Scanner;
+
+public class C07_webSorusu {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen ad giriniz");
+        String ad = scan.nextLine();
+        String harf1 = ad.substring(0, 1);
+        String harf2 = ad.substring(1, 2);
+        String harf3 = ad.substring(2, 3);
+
+        if (ad.length() != 3) {
+            System.out.println("adın uzunlugu 3 olmalidir");
+
+        } else {
+            if (harf1.equals(harf2) || harf1.equals(harf3) || harf2.equals(harf3)) {
+                System.out.println("isim ayni karakterlere sahiptir.");
+
+            } else {
+                System.out.println("Dizenin benzersiz karakterleri var");
+
+            }
+        }
+    }
+}
