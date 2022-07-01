a59987975658edd6b5697aa37256fc4ca68d4ca9
akinalkan <103288322+akinalkan@users.noreply.github.com>
2022-07-02 01:28:01 +0300

    day15

--- src/day14_MethodCreation/C01_MethodCreation.java
+++ src/day14_MethodCreation/C01_MethodCreation.java
@@ -7,14 +7,18 @@ public class C01_MethodCreation {
         //input olarak verilen dort harfli bir
         // stringi tersten yazdiran bir method olusturalim
         terstenYazdir("AKIN");
-        C04_MethodCreation.topla(6,5);
+        C04_MethodCreation.topla(6, 5);
         C02_MethodCreation.rakamlariTopla(563);
 
     }
-    public static void terstenYazdir(String input){
 
-        String tersInput=input.substring(3)+input.substring(2,3)+input.substring(1,2)+input.substring(0,1);
-        System.out.println("Verilen kelimenin tersten yazilisi: "+tersInput);
+    public static void terstenYazdir(String input) {
+
+        String tersInput = input.substring(3) +
+                input.substring(2, 3) +
+                input.substring(1, 2) +
+                input.substring(0, 1);
+        System.out.println("Verilen kelimenin tersten yazilisi: " + tersInput);
 
     }
 }
---
+++ src/day15_overLoadingForLoop/C10_FaizUygulamasi.java
@@ -0,0 +1,35 @@
+package day15_overLoadingForLoop;
+
+import java.util.Scanner;
+
+public class C10_FaizUygulamasi {
+    public static void main(String[] args) {
+         /*
+        Faiz Uygulaması
+        Kullanıcıdan anapara değerini ve parasını kaç yıl vadeli yatırmak istediği
+        bilgisini alın ve her sene sonunda toplam para miktarını ekrana yazdırın.
+
+        Faiz Oranı : %6 Faiz Oranı
+
+        */
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bankamiza hosgeldiniz.Fazi orani %6");
+        int anapara, vade;
+        System.out.print("Yatirmak istediginiz tutar: ");
+        anapara = scan.nextInt();
+        System.out.print("Paranizi kac yil vadelli yatirmak istiyorsunuz: ");
+        vade = scan.nextInt();
+
+        double toplampara = anapara;
+        double faizOrani = 0.06;
+
+
+        for (int i = 1; i <= vade; i++) {
+            toplampara = (toplampara * faizOrani) + toplampara;
+
+            System.out.println(i + "yilin sonunda toplam para : " + (int)toplampara);
+        }
+
+
+    }
+}
